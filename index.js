231d856d2862dc41eaab0c29fea332743021b4aa
minhazuddin10 <minhaz112922@gmail.com>
2021-11-18 16:28:59 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,152 @@
+ const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2otfv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("clockInformation");
+    const servicesCollection = database.collection("service");
+    const ordersCollection = database.collection("users");
+    const reviewCollection = database.collection("review");
+
+    // get api
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    // get single id
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await servicesCollection.findOne(query);
+      res.json(service);
+    });
+    // get api
+    app.get("/review", async (req, res) => {
+      const cursor = reviewCollection.find({});
+      const review = await cursor.toArray();
+      res.send(review);
+    });
+    // post api
+    app.post("/services", async (req, res) => {
+      const service = req.body;
+      const result = await servicesCollection.insertOne(service);
+      res.json(result);
+    });
+
+    // post api
+    app.post("/review", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.json(result);
+    });
+
+    // delete api
+    app.delete("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await servicesCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    // get api for orders
+    app.get("/orders/:name", async (req, res) => {
+      const name = req.params.name;
+      const query = { name: name };
+      const result = await servicesCollection.findOne(query);
+      res.json(result);
+    });
+
+    // order api
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      order.createdAt = new Date();
+      const result = await ordersCollection.insertOne(order);
+      res.json("result");
+    });
+    // get api for orders
+    app.get("/myorders/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await ordersCollection.find(query).toArray();
+      res.json(result);
+    });
+    app.get("/allOrders", async (req, res) => {
+      const result = await ordersCollection.find({}).toArray();
+      res.json(result);
+
+      // delete api
+      app.delete("/myorders/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await ordersCollection.deleteOne(query);
+        res.json(result);
+      });
+    });
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await ordersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = ordersCollection.insertOne(user);
+      console.log(result);
+      res.json(result);
+    });
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await ordersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+    // verifyToken
+    app.put("/users/admin", async (req, res) => {
+      const user = req.body;
+      console.log("put", user);
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: "admin" } };
+      const result = await ordersCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    });
+  } finally {
+    // await client.close()
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("this is backend testing");
+});
+
+app.listen(port, () => {
+  console.log("running port is : ", port);
+});
