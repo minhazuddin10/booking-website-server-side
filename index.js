12ee8bd542db8f4d9ec2b9107125ce34b294d46f
minhazuddin10 <minhaz112922@gmail.com>
2021-11-19 17:24:27 +0600

    uri set up

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2otfv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ofxwj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+console.log(uri)
 async function run() {
   try {
     await client.connect();
@@ -128,7 +128,6 @@ async function run() {
       );
       res.json(result);
     });
-    // verifyToken
     app.put("/users/admin", async (req, res) => {
       const user = req.body;
       console.log("put", user);
@@ -144,7 +143,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("this is backend testing");
+  res.send("backend start");
 });
 
 app.listen(port, () => {
